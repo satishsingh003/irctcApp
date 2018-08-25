409d9ff3b2f4278cfc37c659a0c93d49ce43ab2b
Satish <satishsinghoct87@gmail.com>
2018-08-25 22:22:01 +0530

    Irctc Form

---
+++ src/irctcForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+class Irctcform extends React.Component{
+ 
+  constructor (props, context) {
+    super(props, context);
+  }
+  render () {
+    return (
+      <div>
+          Irctc Form
+      </div>
+    );
+  }
+ 
+}
+ 
+export default Irctcform;
